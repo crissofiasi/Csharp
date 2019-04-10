66d0f6b3885314d85aedaa08adef6f592cb27a62
crissofiasi <crissofiasi@gmail.com>
2019-04-10 18:46:12 +0300

    Keyboard navigation !

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
-using System.Reflection;
-using System.Diagnostics;
 
 namespace WpfApp1
 {
@@ -29,13 +19,17 @@ namespace WpfApp1
             public List<SubItem> listLeft;
             public List<SubItem> listRight;
         }
-        DataLists dataLists=new DataLists();
-        public List<SubItem>  GetFileList(string DirectoryPath)
+
+        private DataLists dataLists = new DataLists();
+        public List<SubItem> GetFileList(string DirectoryPath)
         {
             List<SubItem> list = new List<SubItem>();
             DirectoryInfo currentDir = new DirectoryInfo(DirectoryPath);
             if (!currentDir.Exists)
+            {
                 return null;
+            }
+
             try
             {
                 currentDir.GetDirectories();
@@ -48,15 +42,20 @@ namespace WpfApp1
 
             if (currentDir.Parent != null)
             {
-            SubItem first = new SubItem(currentDir.Parent, "..");
-            
+                SubItem first = new SubItem(currentDir.Parent, "..");
+
                 list.Add(first);
 
             }
             foreach (DirectoryInfo dir in currentDir.GetDirectories())
-                list.Add( new SubItem(dir) );
+            {
+                list.Add(new SubItem(dir));
+            }
+
             foreach (FileInfo file in currentDir.GetFiles())
+            {
                 list.Add(new SubItem(file));
+            }
 
             return list;
         }
@@ -82,32 +81,38 @@ namespace WpfApp1
         private void ClearWindow()
         {
             foreach (TextBlock tb in GrdMain.Children.OfType<TextBlock>())
+            {
                 tb.Text = "";
-            foreach (TextBox txt in GrdMain.Children.OfType<TextBox>())
-                txt.Text = "";
+            }
 
+            foreach (TextBox txt in GrdMain.Children.OfType<TextBox>())
+            {
+                txt.Text = "";
+            }
         }
 
         private void PopulateDriveComboBoxes()
         {
 
             foreach (DriveInfo di in DriveInfo.GetDrives())
+            {
                 foreach (ComboBox cb in GrdMain.Children.OfType<ComboBox>())
+                {
                     cb.Items.Add(di);
-
+                }
+            }
 
             foreach (ComboBox cb in GrdMain.Children.OfType<ComboBox>())
+            {
                 foreach (object cbi in cb.Items)
+                {
                     if (((DriveInfo)cbi).IsReady)
                     {
                         cb.SelectedItem = cbi;
                         break;
                     }
-
-
-
-
-
+                }
+            }
         }
 
         private void CmbDrive_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -122,20 +127,36 @@ namespace WpfApp1
             }
 
             DisplayDriveVolumeLabel(sender);
-           // DisplayPath(sender);
+            // DisplayPath(sender);
             PopulateDataGrid(sender);
         }
+
+
+
+
+
+
 
         private void PopulateDataGrid(object sender)
         {
             ComboBox cb = (ComboBox)sender;
-
             DataGrid dg = SelectFilelistDataGrid(sender, ((ComboBox)sender).Name);
-
             dg.ItemsSource = GetFileList(((DriveInfo)cb.SelectedItem).RootDirectory.FullName);
+            FocusOnDatagridCelll(dg, 0, 0);
         }
 
-        private void PopulateDataGrid(object sender,string path)
+        private static void FocusOnDatagridCelll(DataGrid dg, int row, int column)
+        {
+            dg.Focus();
+            dg.SelectedIndex = row;
+            if ((dg.Items.Count > row) && (dg.Columns.Count > column))
+            {
+                dg.CurrentCell = new DataGridCellInfo(dg.Items[row], dg.Columns[column]);
+
+            }
+        }
+
+        private void PopulateDataGrid(object sender, string path)
         {
             Control cb = (Control)sender;
             DataGrid dg = SelectFilelistDataGrid(sender, ((Control)sender).Name);
@@ -146,8 +167,9 @@ namespace WpfApp1
                 return;
             }
             dg.ItemsSource = fileList;
+            FocusOnDatagridCelll(dg, 0, 0);
 
-            DisplayPath(sender);
+            //   DisplayPath(sender);
 
         }
 
@@ -157,7 +179,7 @@ namespace WpfApp1
         {
             DataGrid dg = SelectFilelistDataGrid(sender, ((Control)sender).Name);
 
-          
+
             TextBlock tb = SelectPathTextBlock(sender, ((Control)sender).Name);
             dg.SelectedItem = dg.Items[0];
             SubItem sb = (SubItem)dg.SelectedItem;
@@ -171,35 +193,45 @@ namespace WpfApp1
             tb.Text = ((DriveInfo)cb.SelectedItem).VolumeLabel.ToString();
         }
 
-
-        DataGrid  SelectFilelistDataGrid(object sender, string name)
+        private DataGrid SelectFilelistDataGrid(object sender, string name)
         {
-            string nm = name.Replace("Cmb", "").Replace("Drive", "").Replace("dg","");
+            string nm = name.Replace("Cmb", "").Replace("Drive", "").Replace("dg", "");
             foreach (DataGrid dg in ((Grid)(((Control)sender).Parent)).Children.OfType<DataGrid>())
-                if (dg.Name.Contains(nm) )
+            {
+                if (dg.Name.ToLower().Contains(nm.ToLower()))
+                {
                     return dg;
+                }
+            }
+
             return null;
         }
 
-
-
-
-
-        TextBlock SelectVolumeLabelTextBlock(object sender,string name)
+        private TextBlock SelectVolumeLabelTextBlock(object sender, string name)
         {
             string nm = name.Replace("Cmb", "").Replace("Drive", "");
             foreach (TextBlock tbl in ((Grid)(((Control)sender).Parent)).Children.OfType<TextBlock>())
+            {
                 if (tbl.Name.Contains(nm) && tbl.Name.ToLower().Contains("name"))
+                {
                     return tbl;
+                }
+            }
+
             return null;
         }
 
-        TextBlock SelectPathTextBlock(object sender, string name)
+        private TextBlock SelectPathTextBlock(object sender, string name)
         {
             string nm = name.Replace("Cmb", "").Replace("Drive", "").Replace("dg", "");
             foreach (TextBlock tbl in ((Grid)(((Control)sender).Parent)).Children.OfType<TextBlock>())
+            {
                 if (tbl.Name.Contains(nm) && tbl.Name.ToLower().Contains("path"))
+                {
                     return tbl;
+                }
+            }
+
             return null;
         }
 
@@ -210,7 +242,8 @@ namespace WpfApp1
 
         private void dg_GotFocus(object sender, RoutedEventArgs e)
         {
-            TblComandPath.Text = SelectPathTextBlock(sender, ((DataGrid)sender).Name).Text;
+            //TblComandPath.Text = SelectPathTextBlock(sender, ((DataGrid)sender).Name).Text;
+            //((Control)sender).Focus();
         }
 
         private void dg_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -224,17 +257,73 @@ namespace WpfApp1
             if (selectedItem.IsDirectory())
             {
                 PopulateDataGrid(sender, selectedItem.FullName);
+
+                //((DataGrid)sender).Focus();
+                ((DataGrid)sender).SelectedIndex = 0;
             }
             else
+            {
                 Process.Start(selectedItem.FullName.ToString());
-        }
-
-        private void dgKeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter) { 
-                HandleDblClickOrEnterPressed(sender);
-
             }
         }
+
+
+
+        private void dgKeyDn(object sender, KeyEventArgs e)
+        {
+
+
+            if (e.Key.Equals(Key.Return))
+            {
+                HandleDblClickOrEnterPressed(sender);
+                e.Handled = true;
+                return;
+
+            }
+
+            if (e.Key.Equals(Key.Tab))
+            {
+                HandleTabPressedInDatagrid(sender);
+
+            }
+
+
+        }
+
+        
+
+        private void HandleTabPressedInDatagrid(object sender)
+        {
+            DataGrid opositeDataGrid;
+            string side = ((DataGrid)sender).Name.Replace("dg", "");
+            string opositeSide = GetOpositeSide(side);
+            opositeDataGrid = SelectFilelistDataGrid(sender, opositeSide);
+            int row=opositeDataGrid.SelectedIndex;
+            if (row < 0)
+                row = 0;
+            FocusOnDatagridCelll(opositeDataGrid, row, 0);
+        }
+
+        private static string GetOpositeSide(string side)
+        {
+            string opositeSide;
+            switch (side.ToLower())
+            {
+                case "left":
+                    opositeSide = "right";
+                    break;
+                case "right":
+                    opositeSide = "left";
+                    break;
+                default:
+                    opositeSide = "";
+                    break;
+
+
+            }
+
+            return opositeSide;
+        }
     }
+
 }
